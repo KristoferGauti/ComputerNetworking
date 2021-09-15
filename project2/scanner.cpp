553a13054f9ef2cbe80c12a9f9bdf4b0704ec208
Kristofer <kristofergauti@gmail.com>
2021-09-15 17:19:08 +0000

    sendto a socket with an address and port 4000 to 4100

--- project2/scanner.cpp
+++ project2/scanner.cpp
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string.h>
 
 
 using namespace std;
@@ -19,23 +20,42 @@ int main(int argc, char* argv[]) {
     char* address = argv[1];
     int from_port_nr = atoi(argv[2]);
     int destination_port_number = atoi(argv[3]);
-    char buffer[1400];
     struct sockaddr_in destaddr;
+    char buffer[1400];
+    strcpy(buffer, "Hi port!");
+    int buffer_length = strlen(buffer) + 1;
 
 
+    //Create a socket
     if (udp_sock < 0) {
         perror("Unable to open socket!");
         exit(0);
     }
 
-
+    //Setup the address
     destaddr.sin_family = AF_INET;
     inet_aton(address, &destaddr.sin_addr);
 
+    destaddr.sin_port = htons(from_port_nr);
+    if (sendto(udp_sock, buffer, buffer_length, 0, (const struct sockaddr*)&destaddr, sizeof(destaddr)) < 0) {
+        perror("Failed");
+    }
+    
+    
 
-    cout << buffer << endl;
-    cout << from_port_nr << endl;
-    cout << destination_port_number << endl;
+
+    for (int port = from_port_nr; port <= destination_port_number; port++) {    
+        if (sendto(udp_sock, buffer, buffer_length, 0, (const struct sockaddr*) &destaddr, sizeof(destaddr)) < 0) {
+            perror("Failed to send!");
+        }
+        else {
+            cout << "sent to a socket!" << endl;
+        }
+        //Receive the udp message from recvfrom then the assignment is done
+        
+        // int length = recvfrom(udp_sock, buffer, buffer_length, 0, NULL, NULL);
+        // cout << length << endl;
+    }
 
     return 0;
 }
