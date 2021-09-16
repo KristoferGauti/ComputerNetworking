787f6f368ea3af8e52327c76e17a5656759b5669
Kristofer <kristofergauti@gmail.com>
2021-09-16 21:59:15 +0000

    Project 2 90% finished

--- project2/scanner.cpp
+++ project2/scanner.cpp
@@ -1,4 +1,4 @@
-//To run the scanner: ./scanner 130.208.242.12 4000 4100
+//To run the scanner: ./scanner 130.208.242.120 4000 4100
 
 #include <iostream>
 #include <sys/socket.h>
@@ -46,28 +46,31 @@ int main(int argc, char* argv[]) {
     tv.tv_usec = 10000; //microseconds
     setsockopt(udp_sock, SOL_SOCKET, SO_RCVTIMEO, (const char*) &tv, sizeof tv);
 
-    destaddr.sin_port = htons(from_port_nr);
     /*
         some packets might be dropped, thus we need to send 5 times in a row to check on that
         inner for loop to do that
     */
-    for (int port = from_port_nr; port <= destination_port_number; port++) {   
+    for (int port = from_port_nr; port <= destination_port_number; port++) {  
+        destaddr.sin_port = htons(port);
         if (sendto(udp_sock, send_buffer, buffer_length, 0, (const struct sockaddr*) &destaddr, sizeof(destaddr)) < 0) {
             perror("Failed to send!");
         }
         else {
-            cout << "Sent to port number ";
-            cout << port << endl;
+            //recvfrom is a blocking function. 
+            recvfrom(udp_sock, receive_buffer, buffer_length, 0, (sockaddr*) &recvaddr, &recv_sock_length); 
+            //cout << receive_buffer << endl;
+
+            //print the port number when a msg is received
+            if (strlen(receive_buffer) > 0) {
+                cout << "port " << port << " is open!" << endl;
+            }
+            
+            //Clear the buffer
+            memset(receive_buffer, 0, sizeof(receive_buffer));
+            
         }
 
-        //recvfrom is a blocking function. 
-        recvfrom(udp_sock, receive_buffer, buffer_length, 0, (sockaddr*) &recvaddr, &recv_sock_length); 
-        cout << receive_buffer << endl;
-        
-        //print the port number when a msg is received
-        if (strlen(receive_buffer) > 0) {
-            cout << "port " << port << " is open!" << endl;
-        }
+
     }
 
     return 0;
