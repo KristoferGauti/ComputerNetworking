2b8fd44b404e12dde0db62e269c74ba85504b28a
Anton Mayboeck <antonmayboeck@gmail.com>
2021-10-26 17:30:51 +0000

    Hello

--- project3/server.cpp
+++ project3/server.cpp
@@ -620,12 +620,12 @@ void serverCommand(int serverSocket, fd_set *openSockets, int *maxfds, char *buf
             std::string group_id = group_IP_portnr_list[i];
             std::string ip_address = group_IP_portnr_list[i + 1];
             std::string port_number = group_IP_portnr_list[i + 2];
-
+            std::cout << 1 << std::endl;
             int sockfd = open_socket(stoi(port_number), false);
-
+            std::cout << 2 << std::endl;
             if (stoi(port_number) != -1 && group_id != "P3_GROUP_7" && !isStored(group_id, stored_names))
             {
-
+                std::cout << 3 << std::endl;
                 if (i == 0)
                 {
                     servers[sockfd] = new Client(sockfd, true);
